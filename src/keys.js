a1c183651cfd1c8b866857e04e5f3bf20d5fdc6b
GermanCode <w.aguirre@udla.edu.co>
2021-03-02 07:36:21 -0500

    p heroku

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
 module.exports = {
     //Database Maquina Local
-    database: {
+    /*database: {
         host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'germath_local'
     }
-
+*/
     //Database Servidor Heroku
-    /*database: {
+    database: {
         host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         user: 'b0ryxd9955wo68hf',
         password: 'x8u24i45jgxbfvbg',
         database: 'aowleewf7faaf7kk'
-    }*/
+    }
 };
