02201baaa36f1db3dcfece0b0e55678e6e356e70
GermanCode <w.aguirre@udla.edu.co>
2021-10-10 23:32:15 -0500

    Informe Final V 2.8

--- dist/keys.js
+++ dist/keys.js
@@ -3,18 +3,19 @@
 //Coleccion de bases de datos tanto para desarrollo como para produccion
 module.exports = {
   //Database Maquina Local
-  /*database: {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'germath_local'
-  } //Database Servidor Heroku
-*/
-  database: {
-      host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-      user: 'b0ryxd9955wo68hf',
-      password: 'x8u24i45jgxbfvbg',
-      database: 'aowleewf7faaf7kk'
-  }
 
+  /*database: {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'germath_local'
+  }
+  */
+  //Database Servidor Heroku
+  database: {
+    host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'b0ryxd9955wo68hf',
+    password: 'x8u24i45jgxbfvbg',
+    database: 'aowleewf7faaf7kk'
+  }
 };
